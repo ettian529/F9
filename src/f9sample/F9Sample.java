31794ea444f1650f28ca0158c229c8de0ff14a7c
B1 <B1@B131>
2017-10-25 13:50:25 +0800

    Commit 1

---
+++ src/f9sample/F9Sample.java
@@ -0,0 +1,13 @@
+
+package f9sample;
+
+
+public class F9Sample {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello");
+    }
+    
+}
